76009e00ecc19adeef6a631c6677ca1aa1e51745
Avkash Mandloi <avkash.mandloi@Yash.com>
2019-11-27 15:50:21 +0530

    Initial files

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    get WebpackConfigExampleDevServer() {
+        return require('./src/webpack.config.example.dev');
+    }
+};
---
+++ src/webpack.config.example.dev.js
@@ -0,0 +1,43 @@
+const webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        contentBase: './examples',
+        disableHostCheck: true,
+        historyApiFallback: true,
+        host: '0.0.0.0',
+        hot: true,
+        port: 8080,
+        stats: {
+            cached: false,
+            cachedAssets: false,
+            colors: true
+        }
+    },
+    mode: 'development',
+    entry: [
+        'react-hot-loader/patch',
+        `webpack-dev-server/client?http://localhost:${port}`,
+        './examples/index.js'
+    ],
+    module: {
+        rules: [
+            {
+                exclude: regex(),
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    output: {
+        'filename': 'app.js',
+        'path': '/examples/',
+        'publicPath': '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin()
+    ]
+};
